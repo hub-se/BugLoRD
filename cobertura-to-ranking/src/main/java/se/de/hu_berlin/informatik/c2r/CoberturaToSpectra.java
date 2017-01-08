c816c576caa4199e9f2ec61c99fea4a919a659ea
heidensi <heiden@informatik.hu-berlin.de>
2017-01-08 15:08:16 +0100

    instrumenting back in own jvm

--- cobertura-to-ranking/src/main/java/se/de/hu_berlin/informatik/c2r/CoberturaToSpectra.java
+++ cobertura-to-ranking/src/main/java/se/de/hu_berlin/informatik/c2r/CoberturaToSpectra.java
@@ -171,56 +171,33 @@ final public class CoberturaToSpectra {
 
 
 		
-//		//build arguments for instrumentation
-//		String[] instrArgs = { 
-//				Instrument.CmdOptions.OUTPUT.asArg(), Paths.get(outputDir).toAbsolutePath().toString()};
-//
-//		if (options.hasOption(CmdOptions.CLASS_PATH)) {
-//			instrArgs = Misc.addToArrayAndReturnResult(instrArgs, 
-//					Instrument.CmdOptions.CLASS_PATH.asArg(), options.getOptionValue(CmdOptions.CLASS_PATH));
-//		}
-//
-//		if (options.getOptionValues(CmdOptions.INSTRUMENT_CLASSES) != null) {
-//			instrArgs = Misc.addToArrayAndReturnResult(instrArgs, Instrument.CmdOptions.INSTRUMENT_CLASSES.asArg());
-//			instrArgs = Misc.joinArrays(instrArgs, options.getOptionValues(CmdOptions.INSTRUMENT_CLASSES));
-//		}
+		//build arguments for instrumentation
+		String[] instrArgs = { 
+				Instrument.CmdOptions.OUTPUT.asArg(), Paths.get(outputDir).toAbsolutePath().toString()};
+
+		if (options.hasOption(CmdOptions.CLASS_PATH)) {
+			instrArgs = Misc.addToArrayAndReturnResult(instrArgs, 
+					Instrument.CmdOptions.CLASS_PATH.asArg(), options.getOptionValue(CmdOptions.CLASS_PATH));
+		}
+
+		if (options.getOptionValues(CmdOptions.INSTRUMENT_CLASSES) != null) {
+			instrArgs = Misc.addToArrayAndReturnResult(instrArgs, Instrument.CmdOptions.INSTRUMENT_CLASSES.asArg());
+			instrArgs = Misc.joinArrays(instrArgs, options.getOptionValues(CmdOptions.INSTRUMENT_CLASSES));
+		}
 
 		final File coberturaDataFile = Paths.get(outputDir, "cobertura.ser").toAbsolutePath().toFile();
 
-//		//sadly, we have no other choice but to start a new java process with the updated class path and the cobertura data file.
-//		//updating the class path on the fly is really messy...
-//		int instrumentationResult = new ExecuteMainClassInNewJVMModule(javaHome, 
-//				Instrument.class, 
-//				classPath, projectDir.toFile(), 
-//				"-Dnet.sourceforge.cobertura.datafile=" + coberturaDataFile.getAbsolutePath().toString())
-//				.submit(instrArgs)
-//				.getResult();
-//
-//		if (instrumentationResult != 0) {
-//			Log.abort(CoberturaToSpectra.class, "Instrumentation failed.");
-//		}
-		
-		
-		
-		Arguments instrumentationArguments;
-		
-		ArgumentsBuilder builder = new ArgumentsBuilder();
-		builder.setDataFile(coberturaDataFile.toString());
-		builder.setDestinationDirectory(instrumentedDir.toString());
-		builder.threadsafeRigorous(true);
-		for (String file : classesToInstrument) {
-			builder.addFileToInstrument(file);
-		}
+		//sadly, we have no other choice but to start a new java process with the updated class path and the cobertura data file.
+		//updating the class path on the fly is really messy...
+		int instrumentationResult = new ExecuteMainClassInNewJVMModule(javaHome, 
+				Instrument.class, 
+				classPath, projectDir.toFile(), 
+				"-Dnet.sourceforge.cobertura.datafile=" + coberturaDataFile.getAbsolutePath().toString())
+				.submit(instrArgs)
+				.getResult();
 
-		instrumentationArguments = builder.build();
-		
-		CodeInstrumentationTask instrumentationTask = new CodeInstrumentationTask();
-		try {
-			ProjectData projectData = new ProjectData();
-			instrumentationTask.instrument(instrumentationArguments, projectData);
-			CoverageDataFileHandler.saveCoverageData(projectData, instrumentationArguments.getDataFile());
-		} catch (Throwable e) {
-			Log.abort(CoberturaToSpectra.class, e, "Error while instrumenting class files.");
+		if (instrumentationResult != 0) {
+			Log.abort(CoberturaToSpectra.class, "Instrumentation failed.");
 		}
 		
 
@@ -264,118 +241,119 @@ final public class CoberturaToSpectra {
 
 	}
 
-//	public final static class Instrument {
-//
-//		private Instrument() {
-//			//disallow instantiation
-//		}
-//
-//		public static enum CmdOptions implements OptionWrapperInterface {
-//			/* add options here according to your needs */
-//			CLASS_PATH("cp", "classPath", true, "An additional class path which may be needed for the execution of tests. "
-//					+ "Will be appended to the regular class path if this option is set.", false),
-//			INSTRUMENT_CLASSES(Option.builder("c").longOpt("classes").required()
-//					.hasArgs().desc("A list of classes/directories to instrument with Cobertura.").build()),
-//			OUTPUT("o", "output", true, "Path to output directory.", true);
-//
-//			/* the following code blocks should not need to be changed */
-//			final private OptionWrapper option;
-//
-//			//adds an option that is not part of any group
-//			CmdOptions(final String opt, final String longOpt, 
-//					final boolean hasArg, final String description, final boolean required) {
-//				this.option = new OptionWrapper(
-//						Option.builder(opt).longOpt(longOpt).required(required).
-//						hasArg(hasArg).desc(description).build(), NO_GROUP);
+	public final static class Instrument {
+
+		private Instrument() {
+			//disallow instantiation
+		}
+
+		public static enum CmdOptions implements OptionWrapperInterface {
+			/* add options here according to your needs */
+			CLASS_PATH("cp", "classPath", true, "An additional class path which may be needed for the execution of tests. "
+					+ "Will be appended to the regular class path if this option is set.", false),
+			INSTRUMENT_CLASSES(Option.builder("c").longOpt("classes").required()
+					.hasArgs().desc("A list of classes/directories to instrument with Cobertura.").build()),
+			OUTPUT("o", "output", true, "Path to output directory.", true);
+
+			/* the following code blocks should not need to be changed */
+			final private OptionWrapper option;
+
+			//adds an option that is not part of any group
+			CmdOptions(final String opt, final String longOpt, 
+					final boolean hasArg, final String description, final boolean required) {
+				this.option = new OptionWrapper(
+						Option.builder(opt).longOpt(longOpt).required(required).
+						hasArg(hasArg).desc(description).build(), NO_GROUP);
+			}
+
+			//adds an option that is part of the group with the specified index (positive integer)
+			//a negative index means that this option is part of no group
+			//this option will not be required, however, the group itself will be
+			CmdOptions(final String opt, final String longOpt, 
+					final boolean hasArg, final String description, final int groupId) {
+				this.option = new OptionWrapper(
+						Option.builder(opt).longOpt(longOpt).required(false).
+						hasArg(hasArg).desc(description).build(), groupId);
+			}
+
+			//adds the given option that will be part of the group with the given id
+			CmdOptions(final Option option, final int groupId) {
+				this.option = new OptionWrapper(option, groupId);
+			}
+
+			//adds the given option that will be part of no group
+			CmdOptions(final Option option) {
+				this(option, NO_GROUP);
+			}
+
+			@Override public String toString() { return option.getOption().getOpt(); }
+			@Override public OptionWrapper getOptionWrapper() { return option; }
+		}
+
+		/**
+		 * @param args
+		 * command line arguments
+		 */
+		public static void main(final String[] args) {
+
+			if (System.getProperty("net.sourceforge.cobertura.datafile") == null) {
+				Log.abort(RunTestsAndGenSpectra.class, "Please include property '-Dnet.sourceforge.cobertura.datafile=.../cobertura.ser' in the application's call.");
+			}
+
+			final OptionParser options = OptionParser.getOptions("Instrument", false, CmdOptions.class, args);
+
+			final String outputDir = options.isDirectory(CmdOptions.OUTPUT, false).toString();
+			final Path coberturaDataFile = Paths.get(System.getProperty("net.sourceforge.cobertura.datafile"));
+			Log.out(Instrument.class, "Cobertura data file: '%s'.", coberturaDataFile);
+
+			final Path instrumentedDir = Paths.get(outputDir, "instrumented").toAbsolutePath();
+			final String[] classesToInstrument = options.getOptionValues(CmdOptions.INSTRUMENT_CLASSES);
+
+			String[] instrArgs = { 
+					"--datafile", coberturaDataFile.toString(),
+					"--destination", instrumentedDir.toString(), 
+					//"--auxClasspath" $COBERTURADIR/cobertura-2.1.1.jar, //not needed since already in class path
+			};
+
+			//add class path for files that can't be found during instrumentation
+			if (options.hasOption(CmdOptions.CLASS_PATH)) {
+				final String[] auxCP = { "--auxClasspath", options.getOptionValue(CmdOptions.CLASS_PATH) };
+				instrArgs = Misc.joinArrays(instrArgs, auxCP);
+			}
+
+			//add the classes (or dirs of classes) to instrument to the end of the argument array
+			instrArgs = Misc.joinArrays(instrArgs, classesToInstrument);
+
+//			//instrument the classes
+//			final int returnValue = InstrumentMain.instrument(instrArgs);
+//			if ( returnValue != 0 ) {
+//				Log.abort(Instrument.class, "Error while instrumenting class files.");
 //			}
-//
-//			//adds an option that is part of the group with the specified index (positive integer)
-//			//a negative index means that this option is part of no group
-//			//this option will not be required, however, the group itself will be
-//			CmdOptions(final String opt, final String longOpt, 
-//					final boolean hasArg, final String description, final int groupId) {
-//				this.option = new OptionWrapper(
-//						Option.builder(opt).longOpt(longOpt).required(false).
-//						hasArg(hasArg).desc(description).build(), groupId);
-//			}
-//
-//			//adds the given option that will be part of the group with the given id
-//			CmdOptions(final Option option, final int groupId) {
-//				this.option = new OptionWrapper(option, groupId);
-//			}
-//
-//			//adds the given option that will be part of no group
-//			CmdOptions(final Option option) {
-//				this(option, NO_GROUP);
-//			}
-//
-//			@Override public String toString() { return option.getOption().getOpt(); }
-//			@Override public OptionWrapper getOptionWrapper() { return option; }
-//		}
-//
-//		/**
-//		 * @param args
-//		 * command line arguments
-//		 */
-//		public static void main(final String[] args) {
-//
-//			if (System.getProperty("net.sourceforge.cobertura.datafile") == null) {
-//				Log.abort(RunTestsAndGenSpectra.class, "Please include property '-Dnet.sourceforge.cobertura.datafile=.../cobertura.ser' in the application's call.");
-//			}
-//
-//			final OptionParser options = OptionParser.getOptions("Instrument", false, CmdOptions.class, args);
-//
-//			final String outputDir = options.isDirectory(CmdOptions.OUTPUT, false).toString();
-//			final Path coberturaDataFile = Paths.get(System.getProperty("net.sourceforge.cobertura.datafile"));
-//			Log.out(Instrument.class, "Cobertura data file: '%s'.", coberturaDataFile);
-//
-//			final Path instrumentedDir = Paths.get(outputDir, "instrumented").toAbsolutePath();
-//			final String[] classesToInstrument = options.getOptionValues(CmdOptions.INSTRUMENT_CLASSES);
-//
-//			String[] instrArgs = { 
-//					"--datafile", coberturaDataFile.toString(),
-//					"--destination", instrumentedDir.toString(), 
-//					//"--auxClasspath" $COBERTURADIR/cobertura-2.1.1.jar, //not needed since already in class path
-//			};
-//
-//			//add class path for files that can't be found during instrumentation
-//			if (options.hasOption(CmdOptions.CLASS_PATH)) {
-//				final String[] auxCP = { "--auxClasspath", options.getOptionValue(CmdOptions.CLASS_PATH) };
-//				instrArgs = Misc.joinArrays(instrArgs, auxCP);
-//			}
-//
-//			//add the classes (or dirs of classes) to instrument to the end of the argument array
-//			instrArgs = Misc.joinArrays(instrArgs, classesToInstrument);
-//
-////			//instrument the classes
-////			final int returnValue = InstrumentMain.instrument(instrArgs);
-////			if ( returnValue != 0 ) {
-////				Log.abort(Instrument.class, "Error while instrumenting class files.");
-////			}
-//			
-//			Arguments instrumentationArguments;
-//			
-//			ArgumentsBuilder builder = new ArgumentsBuilder();
-//			builder.setDataFile(coberturaDataFile.toString());
-//			builder.setDestinationDirectory(instrumentedDir.toString());
-//			for (String file : classesToInstrument) {
-//				builder.addFileToInstrument(file);
-//			}
-//
-//			instrumentationArguments = builder.build();
-//			
-//			CodeInstrumentationTask instrumentationTask = new CodeInstrumentationTask();
-//			try {
-//				ProjectData projectData = new ProjectData();
-//				instrumentationTask.instrument(instrumentationArguments, projectData);
-//				CoverageDataFileHandler.saveCoverageData(projectData, instrumentationArguments.getDataFile());
-//			} catch (Throwable e) {
-//				Log.abort(Instrument.class, e, "Error while instrumenting class files.");
-//			}
-//
-//		}
-//
-//	}
+			
+			Arguments instrumentationArguments;
+			
+			ArgumentsBuilder builder = new ArgumentsBuilder();
+			builder.setDataFile(coberturaDataFile.toString());
+			builder.setDestinationDirectory(instrumentedDir.toString());
+			builder.threadsafeRigorous(true);
+			for (String file : classesToInstrument) {
+				builder.addFileToInstrument(file);
+			}
+
+			instrumentationArguments = builder.build();
+			
+			CodeInstrumentationTask instrumentationTask = new CodeInstrumentationTask();
+			try {
+				ProjectData projectData = new ProjectData();
+				instrumentationTask.instrument(instrumentationArguments, projectData);
+				CoverageDataFileHandler.saveCoverageData(projectData, instrumentationArguments.getDataFile());
+			} catch (Throwable e) {
+				Log.abort(Instrument.class, e, "Error while instrumenting class files.");
+			}
+
+		}
+
+	}
 
 	public final static class RunTestsAndGenSpectra {
 
