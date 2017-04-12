5bbb3a49c2fd103ba2562130dc991250ed96aed7
Sulilol <Sulilol@web.de>
2017-04-12 17:35:32 +0200

    Fixed compilation errors. What is the implementation of the dispatcher now?

--- ast-lm-builder/src/main/java/se/de/hu_berlin/informatik/astlmbuilder/parser/TokenParser.java
+++ ast-lm-builder/src/main/java/se/de/hu_berlin/informatik/astlmbuilder/parser/TokenParser.java
@@ -2,11 +2,12 @@ package se.de.hu_berlin.informatik.astlmbuilder.parser;
 
 import com.github.javaparser.ast.Node;
 import com.github.javaparser.ast.NodeList;
+import com.github.javaparser.ast.body.BodyDeclaration;
+import com.github.javaparser.ast.body.TypeDeclaration;
 
 import se.de.hu_berlin.informatik.astlmbuilder.mapping.keywords.IKeyWordProvider;
 import se.de.hu_berlin.informatik.astlmbuilder.mapping.keywords.KeyWordConstants;
 import se.de.hu_berlin.informatik.astlmbuilder.parser.dispatcher.IKeyWordDispatcher;
-import se.de.hu_berlin.informatik.astlmbuilder.parser.dispatcher.KeyWordDispatcher;
 
 /**
  * A simple implementation of the token parser using the human readable keywords
@@ -14,7 +15,7 @@ import se.de.hu_berlin.informatik.astlmbuilder.parser.dispatcher.KeyWordDispatch
 public class TokenParser implements ITokenParser{
 
 	IKeyWordProvider<String> kwp = new KeyWordConstants();
-	IKeyWordDispatcher kwd = new KeyWordDispatcher();
+	IKeyWordDispatcher kwd = null; // what is the dispatcher now?
 	
 	@Override
 	public IKeyWordProvider<String> getKeyWordProvider() {
@@ -42,6 +43,24 @@ public class TokenParser implements ITokenParser{
 	@Override
 	public <T extends Node> NodeList<T> guessList(Class<T> expectedSuperClazz, InformationWrapper info) {
 		// TODO implement
+		return null;
+	}
+
+	@Override
+	public String guessMethodIdentifier(InformationWrapper info) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public NodeList<BodyDeclaration<?>> guessBodyDeclarationList(InformationWrapper info) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public NodeList<TypeDeclaration<?>> guessTypeDeclarationList(InformationWrapper info) {
+		// TODO Auto-generated method stub
 		return null;
 	}
 
