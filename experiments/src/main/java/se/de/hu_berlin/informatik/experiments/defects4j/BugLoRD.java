37ce2532d44dcdace4af41993f871e5f10cfdf4c
heidensi <heiden@informatik.hu-berlin.de>
2017-05-17 10:59:12 +0200

    keep lm in temp directory as long as possible

--- experiments/src/main/java/se/de/hu_berlin/informatik/experiments/defects4j/BugLoRD.java
+++ experiments/src/main/java/se/de/hu_berlin/informatik/experiments/defects4j/BugLoRD.java
@@ -1,21 +1,26 @@
 package se.de.hu_berlin.informatik.experiments.defects4j;
 
 import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Properties;
 
 import se.de.hu_berlin.informatik.utils.properties.PropertyLoader;
 import se.de.hu_berlin.informatik.utils.properties.PropertyTemplate;
 
 public final class BugLoRD {
-	
+
 	public static enum BugLoRDProperties implements PropertyTemplate {
+		TEMP_DIR("tmp_dir", "/path/to/../tmp", "path to a directory to use for temporary data"),
+
 		SRILM_DIR("srilm_dir", "/path/to/../SRILM-1.7.1/bin",
 				"path to SRILM (not necessarily needed to run experiments with Defects4J)"),
 		KEN_LM_DIR("kenlm_dir", "/path/to/../kenlm/bin",
 				"path to the (altered) kenLM version (needed for the computation of LM rankings)"),
 
-		RANKING_PERCENTAGES("ranking_percentages", "0 2 4 6 8 10 12 14 16 18 20 22 24 26 28 30 "
-				+ "32 34 36 38 40 42 44 46 48 50 52 54 56 58 60 62 64 66 68 70 72 74 76 78 80 82 84 86 88 90 92 94 96 98 100",
+		RANKING_PERCENTAGES("ranking_percentages",
+				"0 2 4 6 8 10 12 14 16 18 20 22 24 26 28 30 "
+						+ "32 34 36 38 40 42 44 46 48 50 52 54 56 58 60 62 64 66 68 70 72 74 76 78 80 82 84 86 88 90 92 94 96 98 100",
 				"specify percentages with which to combine the SBFL and LM rankings (0 means 0% SBFL ranking, 100 means 100%)",
 				"ranking_percentages=0 5 10 15 20 25 30 35 40 45 50 55 60 65 70 75 80 85 90 95 100",
 				"ranking_percentages=0 1 2 3 4 5 10 50 90 95 96 97 98 99 100"),
@@ -24,73 +29,95 @@ public final class BugLoRD {
 				"you can specify different SBFL ranking metrics at this point. This ensures that",
 				"only rankings for these metrics will get generated or plotted when running the",
 				"respecting tools. This way, you don't have to give every tool this list of",
-				"ranking metrics as an option.",
-				"localizers=Op2 GP13 Tarantula Ochiai Jaccard RussellRao",
+				"ranking metrics as an option.", "localizers=Op2 GP13 Tarantula Ochiai Jaccard RussellRao",
 				"localizers=Op2 GP13 Tarantula Ochiai Jaccard Ample Anderberg ArithmeticMean "
-				+ "Cohen Dice Euclid Fleiss GeometricMean Goodman Hamann Hamming HarmonicMean "
-				+ "Kulczynski1 Kulczynski2 M1 M2 Ochiai2 Overlap RogersTanimoto Rogot1 Rogot2 "
-				+ "RussellRao Scott SimpleMatching Sokal SorensenDice Wong1 Wong2 Wong3 Zoltar"),
-				
+						+ "Cohen Dice Euclid Fleiss GeometricMean Goodman Hamann Hamming HarmonicMean "
+						+ "Kulczynski1 Kulczynski2 M1 M2 Ochiai2 Overlap RogersTanimoto Rogot1 Rogot2 "
+						+ "RussellRao Scott SimpleMatching Sokal SorensenDice Wong1 Wong2 Wong3 Zoltar"),
+
 		GLOBAL_LM_BINARY("global_lm_binary", "/path/to/../some_language_model.kenlm.binary",
 				"specify the path to the LM binary that shall be used");
 
 		final private String[] descriptionLines;
 		final private String identifier;
 		final private String placeHolder;
-		
+
 		private String value = null;
-		
+
 		BugLoRDProperties(String identifier, String placeHolder, String... descriptionLines) {
 			this.identifier = identifier;
 			this.placeHolder = placeHolder;
 			this.descriptionLines = descriptionLines;
 		}
 
-		@Override public String getPropertyIdentifier() { return identifier; }
-		@Override public String getPlaceHolder() { return placeHolder; }
-		@Override public String[] getHelpfulDescription() { return descriptionLines; }
+		@Override
+		public String getPropertyIdentifier() {
+			return identifier;
+		}
 
-		@Override public void setPropertyValue(String value) { this.value = value; }
-		@Override public String getValue() { return value; }
+		@Override
+		public String getPlaceHolder() {
+			return placeHolder;
+		}
+
+		@Override
+		public String[] getHelpfulDescription() {
+			return descriptionLines;
+		}
+
+		@Override
+		public void setPropertyValue(String value) {
+			this.value = value;
+		}
+
+		@Override
+		public String getValue() {
+			return value;
+		}
 	}
 
 	private final static String SEP = File.separator;
-	
+
 	public final static String PROP_FILE_NAME = "BugLoRDProperties.ini";
-	
-	private static Properties props = PropertyLoader.loadProperties(new File(BugLoRD.PROP_FILE_NAME), BugLoRDProperties.class);
-	
-	//suppress default constructor (class should not be instantiated)
+
+	private static Properties props = PropertyLoader
+			.loadProperties(new File(BugLoRD.PROP_FILE_NAME), BugLoRDProperties.class);
+
+	// suppress default constructor (class should not be instantiated)
 	private BugLoRD() {
 		throw new AssertionError();
 	}
-	
+
 	public static Properties getProperties() {
 		return props;
 	}
-	
+
 	public static String getValueOf(BugLoRDProperties property) {
 		return property.getValue();
 	}
-	
+
+	public static Path getTmpDir() {
+		return Paths.get(getValueOf(BugLoRDProperties.TEMP_DIR));
+	}
+
 	public static String getSRILMMakeBatchCountsExecutable() {
 		return getValueOf(BugLoRDProperties.SRILM_DIR) + SEP + "make-batch-counts";
 	}
-	
+
 	public static String getSRILMMergeBatchCountsExecutable() {
 		return getValueOf(BugLoRDProperties.SRILM_DIR) + SEP + "merge-batch-counts";
 	}
-	
+
 	public static String getSRILMMakeBigLMExecutable() {
 		return getValueOf(BugLoRDProperties.SRILM_DIR) + SEP + "make-big-lm";
 	}
-	
+
 	public static String getKenLMBinaryExecutable() {
 		return getValueOf(BugLoRDProperties.KEN_LM_DIR) + SEP + "build_binary";
 	}
-	
+
 	public static String getKenLMQueryExecutable() {
 		return getValueOf(BugLoRDProperties.KEN_LM_DIR) + SEP + "query";
 	}
-	
+
 }
--- experiments/src/main/java/se/de/hu_berlin/informatik/experiments/lm/LMBuilder.java
+++ experiments/src/main/java/se/de/hu_berlin/informatik/experiments/lm/LMBuilder.java
@@ -1,19 +1,23 @@
 package se.de.hu_berlin.informatik.experiments.lm;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
+
 import se.de.hu_berlin.informatik.benchmark.api.defects4j.Defects4J;
 import se.de.hu_berlin.informatik.experiments.defects4j.BugLoRD;
 import se.de.hu_berlin.informatik.experiments.lm.BuildLanguageModel.CmdOptions;
 import se.de.hu_berlin.informatik.utils.files.FileUtils;
+import se.de.hu_berlin.informatik.utils.files.processors.FileToStringListReader;
+import se.de.hu_berlin.informatik.utils.files.processors.StringListToFileWriter;
 import se.de.hu_berlin.informatik.utils.miscellaneous.Log;
 import se.de.hu_berlin.informatik.utils.processors.AbstractConsumingProcessor;
 import se.de.hu_berlin.informatik.utils.processors.sockets.ProcessorSocket;
 
 public class LMBuilder extends AbstractConsumingProcessor<Integer> {
 
-	
 	private Path inputDir;
 	private Path output;
 
@@ -21,46 +25,76 @@ public class LMBuilder extends AbstractConsumingProcessor<Integer> {
 		this.inputDir = inputDir;
 		this.output = output;
 	}
-	
+
 	@Override
 	public void consumeItem(Integer order, ProcessorSocket<Integer, Object> socket) {
 		socket.requireOptions();
-		
-		Path temporaryFilesDir = inputDir.resolve("_tempLMDir" + order + "_");
+
+		Path temporaryFilesDir = BugLoRD.getTmpDir().resolve(inputDir.getFileName())
+				.resolve("_tempLMDir" + order + "_");
 		FileUtils.delete(temporaryFilesDir);
-		
-		//file that contains a list of all token files (needed by SRILM)
+
+		// file that contains a list of all token files (needed by SRILM)
 		Path listFile = inputDir.resolve("file.list");
 		if (!listFile.toFile().exists()) {
 			Log.abort(this, "Token list file '%s' does not exist.", listFile);
 		}
-		
-		//make batch counts with SRILM
+
+		// make batch counts with SRILM
 		String countsDir = temporaryFilesDir + Defects4J.SEP + "counts";
 		Paths.get(countsDir).toFile().mkdirs();
-		Defects4J.executeCommand(temporaryFilesDir.toFile(), BugLoRD.getSRILMMakeBatchCountsExecutable(), 
-				listFile.toString(), "10", "/bin/cat", countsDir, "-order", String.valueOf(order), "-unk");
-		
-		//merge batch counts with SRILM
+		Defects4J.executeCommand(
+				temporaryFilesDir.toFile(), BugLoRD.getSRILMMakeBatchCountsExecutable(), listFile.toString(), "10",
+				"/bin/cat", countsDir, "-order", String.valueOf(order), "-unk");
+
+		// merge batch counts with SRILM
 		Defects4J.executeCommand(temporaryFilesDir.toFile(), BugLoRD.getSRILMMergeBatchCountsExecutable(), countsDir);
-		
-		//estimate language model of order n with SRILM
-		String arpalLM = output + "_order" + order + ".arpa";
-		Defects4J.executeCommand(temporaryFilesDir.toFile(), BugLoRD.getSRILMMakeBigLMExecutable(), "-read", 
-				countsDir + Defects4J.SEP + "*.gz", "-lm", arpalLM, "-order", String.valueOf(order), "-unk");
-		
+
+		// estimate language model of order n with SRILM
+		String tempArpalLM = temporaryFilesDir.resolve("tmp_order" + order + ".arpa").toString();
+		Defects4J.executeCommand(
+				temporaryFilesDir.toFile(), BugLoRD.getSRILMMakeBigLMExecutable(), "-read",
+				countsDir + Defects4J.SEP + "*.gz", "-lm", tempArpalLM, "-order", String.valueOf(order), "-unk");
+
+		// log the first 100 lines of the arpa file and its file size
+		logArpaFile(tempArpalLM, output);
+
 		if (socket.getOptions().hasOption(CmdOptions.GEN_BINARY)) {
-			//build binary with kenLM
+			// build binary with kenLM
 			String binaryLM = output + "_order" + order + ".binary";
-			Defects4J.executeCommand(temporaryFilesDir.toFile(), BugLoRD.getKenLMBinaryExecutable(),
-					arpalLM, binaryLM);
+			Defects4J.executeCommand(
+					temporaryFilesDir.toFile(), BugLoRD.getKenLMBinaryExecutable(), tempArpalLM, binaryLM);
 			if (new File(binaryLM).exists() && !socket.getOptions().hasOption(CmdOptions.KEEP_ARPA)) {
-				FileUtils.delete(new File(arpalLM));
+				FileUtils.delete(new File(tempArpalLM));
+			} else {
+				copyLM(order, tempArpalLM);
 			}
+		} else {
+			copyLM(order, tempArpalLM);
 		}
-		
-		//delete the temporary LM files
+
+		// delete the temporary LM files
 		FileUtils.delete(temporaryFilesDir);
+	}
+
+	private void logArpaFile(String tempArpalLM, Path output) {
+		File arpa = new File(tempArpalLM);
+
+		if (arpa.exists()) {
+			Path logOutput = output.resolve("arpaLog");
+			List<String> result = new FileToStringListReader(0, 100).submit(arpa.toPath()).getResult();
+			new StringListToFileWriter<>(logOutput.resolve(arpa.getName() + "-" + arpa.length() + ".log"), true).submit(result);
+		}
+	}
+
+	private void copyLM(Integer order, String tempArpalLM) {
+		String arpaLM = output + "_order" + order + ".arpa";
+		try {
+			FileUtils.copyFileOrDir(new File(tempArpalLM), new File(arpaLM));
+			FileUtils.delete(new File(tempArpalLM));
+		} catch (IOException e) {
+			Log.abort(this, "Could not copy '%s' to '%s'.", tempArpalLM, arpaLM);
+		}
 	}
 
 }
