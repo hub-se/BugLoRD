1dcf28282da50b2e2ced99a7be5bb11951d07ef9
Simon Heiden <heiden@informatik.hu-berlin.de>
2019-12-05 19:06:17 +0100

    test runner update

--- trace-cobertura/src/main/java/se/de/hu_berlin/informatik/spectra/provider/tracecobertura/infrastructure/BufferedMap.java
+++ trace-cobertura/src/main/java/se/de/hu_berlin/informatik/spectra/provider/tracecobertura/infrastructure/BufferedMap.java
@@ -35,7 +35,7 @@ public class BufferedMap<E> implements Map<Integer, E>, Serializable {
 	private static final long serialVersionUID = 3786896457427890598L;
 
 	// keep at most 3 nodes in memory
-    private static final int CACHE_SIZE = 3;
+    private static final int CACHE_SIZE = 4;
     
 	private File output;
 	private String filePrefix;
