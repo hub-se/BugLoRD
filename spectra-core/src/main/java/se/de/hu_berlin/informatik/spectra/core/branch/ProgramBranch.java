ef086f690e279701a4bb2677ec4dc76ed0f5f0c9
heidensi <heiden@informatik.hu-berlin.de>
2020-06-02 12:04:34 +0200

    use iterator for better performance

--- spectra-core/src/main/java/se/de/hu_berlin/informatik/spectra/core/branch/ProgramBranch.java
+++ spectra-core/src/main/java/se/de/hu_berlin/informatik/spectra/core/branch/ProgramBranch.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -48,15 +49,57 @@ public class ProgramBranch implements Shortened, Comparable<ProgramBranch>, Inde
     public String toString() {
 		StringBuilder builder = new StringBuilder();
         builder.append(this.id);
-        Collection<SourceCodeBlock> branchElements = getElements();
-		for (SourceCodeBlock block : branchElements) {
+        Iterator<SourceCodeBlock> branchElements = Iterator();
+        while (branchElements.hasNext()) {
             builder.append(IDENTIFIER_SEPARATOR_CHAR)
-            .append(block.toString());
+            .append(branchElements.next().toString());
         }
         return builder.toString();
     }
 
-    @Override
+    private Iterator<SourceCodeBlock> Iterator() {
+    	int[] subTraceSequence = programBranchSpectra.getSubTraceSequenceMap().get(id);
+    	if (subTraceSequence == null) {
+    		return null;
+    	}
+    	CachedMap<int[]> nodeSequenceMap = programBranchSpectra.getNodeSequenceMap();
+    	CachedMap<SourceCodeBlock> statementMap = programBranchSpectra.getStatementMap();
+    	
+    	return new Iterator<SourceCodeBlock>() {
+    		int subTraceIndex = 0; // inner index
+    		int subTraceSequenceIndex = 0; // outer index
+            int[] currentNodeIdSequence;
+
+            @Override
+            public boolean hasNext() {
+            	// need new sub trace? look for the next valid sub trace
+            	if (currentNodeIdSequence == null || subTraceIndex >= currentNodeIdSequence.length) {
+            		// try to get the next valid sub trace!
+            		while (subTraceSequenceIndex < subTraceSequence.length) {
+            			// get the next sub trace from the current sub trace sequence
+            			currentNodeIdSequence = nodeSequenceMap.get(subTraceSequence[subTraceSequenceIndex++]);
+            			if (currentNodeIdSequence.length > 0) {
+            				// found a "good" sequence
+            				//                        	System.out.println("sub seq start");
+            				subTraceIndex = 0;
+            				break;
+            			}
+            			currentNodeIdSequence = null;
+            		}
+            	}
+
+            	// if we found a valid sub trace, it should be non-null
+            	return currentNodeIdSequence != null;
+            }
+
+            @Override
+            public SourceCodeBlock next() {
+                return statementMap.get(currentNodeIdSequence[subTraceIndex++]);
+            }
+		};
+	}
+
+	@Override
     public int hashCode() {
         return 31 * (527 + this.id);
     }
@@ -185,8 +228,7 @@ public class ProgramBranch implements Shortened, Comparable<ProgramBranch>, Inde
     	
     	int[] subTraceSequence = programBranchSpectra.getSubTraceSequenceMap().get(id);
     	if (subTraceSequence == null) {
-//    		throw new IllegalStateException("No branch found for id " + this.id);
-    		return Collections.emptyList();
+    		return null;
     	}
     	CachedMap<int[]> nodeSequenceMap = programBranchSpectra.getNodeSequenceMap();
     	
