e23a51eb6c6c1b5e944af5ca14a7cc4bd89af339
Philipp Thamm <thammphi@student.hu-berlin.de>
2020-12-04 14:35:02 +0100

    made decrement smaller so not all confidence scores become 0.0

--- fault-localization-toolset/src/main/java/se/de/hu_berlin/informatik/faultlocalizer/ngram/Nessa.java
+++ fault-localization-toolset/src/main/java/se/de/hu_berlin/informatik/faultlocalizer/ngram/Nessa.java
@@ -54,7 +54,7 @@ public class Nessa<T> extends AbstractFaultLocalizer<T> {
         	//System.out.println("_________________"); //PT
         	//System.out.println("Failed tests: {" + hitTrace.getFailedTest() + "}");
         System.out.println("_________________"); //PT
-        System.out.println("Failed tests (PT method): {" + hitTrace.getFailedTestTraces() + "}");
+        	//System.out.println("Failed tests (PT method): {" + hitTrace.getFailedTestTraces() + "}");
         	//System.out.println("_________________"); //PT
         	//System.out.println("All tests: {" + hitTrace.getTestTrace() + "}");
         System.out.println("Confidence: " + nGrams.getConfidence()); //PT
@@ -81,7 +81,7 @@ public class Nessa<T> extends AbstractFaultLocalizer<T> {
     			hitTrace.getSuccessfulTestTraces().forEach(successfulTestTrace -> {
     				successfulTestTrace.getInvolvedBlocks().forEach(blockID -> {
     					if (ID == blockID) {
-    						nGram.setConfidence(nGram.getConfidence() - 0.1);
+    						nGram.setConfidence(nGram.getConfidence() - 0.0001);
     					}
     				});
     			});
