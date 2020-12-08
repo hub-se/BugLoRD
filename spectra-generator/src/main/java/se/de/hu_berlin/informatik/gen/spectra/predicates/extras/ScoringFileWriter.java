1dd15e72a22d59e419eca6e668333a0772af4511
Roman <roman@displayer.info>
2020-12-08 02:57:34 +0100

    extend scoring

--- spectra-generator/src/main/java/se/de/hu_berlin/informatik/gen/spectra/predicates/extras/ScoringFileWriter.java
+++ spectra-generator/src/main/java/se/de/hu_berlin/informatik/gen/spectra/predicates/extras/ScoringFileWriter.java
@@ -3,6 +3,7 @@ package se.de.hu_berlin.informatik.gen.spectra.predicates.extras;
 import se.de.hu_berlin.informatik.benchmark.api.defects4j.Defects4J;
 import se.de.hu_berlin.informatik.utils.miscellaneous.Log;
 
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 
@@ -12,10 +13,10 @@ public class ScoringFileWriter {
 
     public synchronized void write(String line) {
         try {
-            FileWriter fw = new FileWriter(Defects4J.Defects4JProperties.ARCHIVE_DIR.getValue() + "/result.csv");
-            fw.append(line);
-            fw.flush();
-            fw.close();
+            FileOutputStream fos = new FileOutputStream(Defects4J.Defects4JProperties.ARCHIVE_DIR.getValue() + "/result.csv", true);
+            fos.write(line.getBytes());
+            fos.flush();
+            fos.close();
         }
         catch (IOException ex) {
            Log.err(ex, "IOException while writing to file");
