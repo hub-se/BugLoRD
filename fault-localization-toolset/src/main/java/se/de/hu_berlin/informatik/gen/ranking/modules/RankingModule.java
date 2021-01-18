6be488ecc619e8eb9d95633cd4294038e3f736d6
Philipp Thamm <thammphi@student.hu-berlin.de>
2021-01-18 19:44:23 +0100

    save file under new name

--- fault-localization-toolset/src/main/java/se/de/hu_berlin/informatik/gen/ranking/modules/RankingModule.java
+++ fault-localization-toolset/src/main/java/se/de/hu_berlin/informatik/gen/ranking/modules/RankingModule.java
@@ -88,7 +88,7 @@ public class RankingModule<T> extends AbstractProcessor<ISpectra<T, ?>, ISpectra
             Paths.get(outputdir + File.separator + subfolder).toFile().mkdirs();
             ranking.save(outputdir + File.separator + subfolder + File.separator + BugLoRDConstants.FILENAME_RANKING_FILE);
             System.out.println("------- ranking saved -------"); //PT
-            ranking.save(outputdir + File.separator + subfolder + File.separator + BugLoRDConstants.FILENAME_RANKING_FILE);
+            ranking.save(outputdir + File.separator + subfolder + File.separator + "ranking2.rnk");
             System.out.println("------- ranking saved again -------"); //PT
             System.out.println(ranking.getBestRankingElement()); //PT
             System.out.println(ranking.getBestRankingValue()); //PT
